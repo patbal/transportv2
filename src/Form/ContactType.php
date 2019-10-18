ee3858a9fc226543fe9242ea4033047a6c4651b8
p.balland <p.balland@matchevent.fr>
2019-10-18 12:03:05 +0200

    Ajout des liens adresse - contacts modif de l'un depuis l'autre

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Adresse;
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +20,19 @@ class ContactType extends AbstractType
             ->add('prenom')
             ->add('telephone')
             ->add('mail')
+            ->add('societe', entityType::class, array(
+                'class' => Adresse::class,
+                'query_builder' => function(entityRepository $er){
+                    return $er -> createQueryBuilder('u')
+                                -> orderBy('u.nom', 'ASC')
+                ;},
+                'choice_label' => 'nom',
+                'choice_value' => 'id',
+                'placeholder' => '---',
+                'multiple' => false,
+                'expanded' => false,
+                'label' => 'société / site'
+            ))
             ->add('sauvegarder',submitType::class)
         ;
     }
